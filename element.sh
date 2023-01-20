2cd0d3244d43a298e4ce33c8576490954c4fae41
DocOc98 <h.o.delavega@gmail.com>
2023-01-20 03:55:55 +0000

    fix: showing wrong message

--- element.sh
+++ element.sh
@@ -10,8 +10,8 @@ else
   ELEMENTS_RESULT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, types.type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
   if [[ -z $ELEMENTS_RESULT ]]
   then
-    echo $ELEMENTS_RESULT #| read ATOMIC_NUMBER
-  else
     echo "I could not find that element in the database."
+  else
+    echo $ELEMENTS_RESULT #| read ATOMIC_NUMBER
   fi
 fi
