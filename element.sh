89768dade49717a3044b48ccc14f399e7999df60
DocOc98 <h.o.delavega@gmail.com>
2023-01-20 03:53:23 +0000

    test: add for another elements

--- element.sh
+++ element.sh
@@ -6,4 +6,12 @@ then
 elif [[ $1 == 1 || $1 == "H" || $1 == "Hydrogen" ]]
 then
   echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+else
+  ELEMENTS_RESULT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, types.type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")
+  if [[ -z $ELEMENTS_RESULT ]]
+  then
+    echo $ELEMENTS_RESULT #| read ATOMIC_NUMBER
+  else
+    echo "I could not find that element in the database."
+  fi
 fi
